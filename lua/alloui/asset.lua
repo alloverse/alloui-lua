fa1264e4d340c827df46777f3d0211cd4f1eb3b2
Patrik Sjöberg <voxxar@gmail.com>
2021-01-28 00:46:50 +0100

    Add a Base64Asset

--- lua/alloui/asset.lua
+++ lua/alloui/asset.lua
@@ -157,6 +157,27 @@ function Asset:id(refresh)
     return self._id
 end
 
+Base64Asset = class(Asset)
+
+function Base64Asset:_init(base64)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' -- You will need this for encoding/decoding
+    base64 = string.gsub(base64, '[^'..b..'=]', '')
+    local data = (base64:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+            return string.char(c)
+    end))
+    self:super(data)
+end
+
+
+
 FileAsset = class(Asset)
 
 function FileAsset:_init(path)
