1f9992ae71546c019d61f07467e217c462f6f221
Nevyn Bengtsson <nevyn@alloverse.com>
2021-02-27 19:06:52 +0100

    allow file asset to read data locally
    if you need to actually use the asset for local content, e g in the visor :)

--- lua/alloui/asset/file_asset.lua
+++ lua/alloui/asset/file_asset.lua
@@ -6,9 +6,12 @@ local Asset = require ('alloui.asset.asset')
 
 FileAsset = class.FileAsset(Asset)
 
-function FileAsset:_init(path)
+function FileAsset:_init(path, load)
     self._path = path
     self._file = assert(io.open(path, "r+b"))
+    if load then
+        self.data = self:read(1, self:size())
+    end
 end
 
 function FileAsset:path()
