c1c8686d00710c85be5f833b3af2cf73d8688124
Patrik Sjöberg <voxxar@gmail.com>
2021-05-17 11:20:29 +0200

    Also do horizontal

--- lua/alloui/views/verticalstackview.lua
+++ lua/alloui/views/verticalstackview.lua
@@ -10,11 +10,19 @@ local Pose = require(modules .."pose")
 local View = require(modules .."views.view")
 
 
-class.VerticalStackView(View)
-function VerticalStackView:_init(bounds)
+class.VerticalStackView(Surface)
+function VerticalStackView:_init(bounds, axis)
     self:super(bounds)
-
-    self._margin = 0.01
+    self._margin = 0.05
+    self.color = {1, 0, 0, 1}
+    if axis and axis:sub(1, 1) == "h" then
+        self.onAxis = vec3(1, 0, 0)
+        self.offAxis = vec3(0, 1, 0)
+        self.color = {0, 1, 0, 1}
+    else
+        self.onAxis = vec3(0, 1, 0)
+        self.offAxis = vec3(1, 0, 0)
+    end
 end
 
 function VerticalStackView:margin(newValue)
@@ -25,21 +33,47 @@ function VerticalStackView:margin(newValue)
     return self._margin
 end
 
-function VerticalStackView:layout()
+function VerticalStackView:layout(guard)
     if #self.subviews == 0 then return end
+    local onAxis = self.onAxis
+    local offAxis = self.offAxis
 
-    local width = self.bounds.size.width
-    local pen = Pose(0, self.bounds.size.height/2, 0)
+    local margin = vec3(self._margin, self._margin) * onAxis
+    local offAxisSize = vec3(self.bounds.size.width, self.bounds.size.height, 0) * offAxis
+    local pen = Pose(0, 0, 0)
+    -- set y positions
     for i, v in ipairs(self.subviews) do
-        pen:move(0, -v.bounds.size.height/2, 0)
+        local size = vec3(v.bounds.size.width, v.bounds.size.height, 0) * onAxis + offAxisSize
+        local offset = -(size * onAxis) / 2
+        pen:move(offset.x, offset.y, 0)
         v.bounds.pose = pen:copy()
-        v.bounds.size.width = width
-        if v.layout then 
-            v:layout()
-        end
-        v:updateComponents()
-        pen:move(0, -v.bounds.size.height/2 - self._margin, 0)
+        v.bounds.size.width = size.x
+        v.bounds.size.height = size.y
+        pen:move(offset.x - margin.x, offset.y - margin.y, 0)
     end
+    pen:move(margin.x, margin.y, 0)
+    
+    -- calculate new stack height
+    local height = -((pen.transform * vec3()) * onAxis) + offAxisSize
+    self.bounds.size.width = height.x
+    self.bounds.size.height = height.y
+    
+    -- Offset all subviews as all positions are from center. Commit final sizes
+    pen = Pose((height * onAxis).x / 2, (height * onAxis).y / 2, 0)
+    for i, v in ipairs(self.subviews) do
+        local size = vec3(v.bounds.size.width, v.bounds.size.height, 0) * onAxis + offAxisSize
+        local offset = -(size * onAxis) / 2
+        pen:move(offset.x, offset.y, 0)
+        v.bounds.pose = pen:copy()
+        v.bounds.size.width = size.x
+        v.bounds.size.height = size.y
+        if v.layout then
+            v:layout()
+            v:updateComponents()
+        end
+        pen:move(offset.x - margin.x, offset.y - margin.y, 0)
+    end
+    self:updateComponents()
 end
 
 return VerticalStackView
