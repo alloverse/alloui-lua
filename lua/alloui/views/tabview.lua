2854f15daa98332cc64659c787e0c644dbefe672
Nevyn Bengtsson <nevyn@alloverse.com>
2022-01-18 20:58:22 +0100

    TabView: Colors, bug fixes, ...

--- lua/alloui/views/tabview.lua
+++ lua/alloui/views/tabview.lua
@@ -20,10 +20,6 @@ function TabViewItem:_init(view, name, button)
     self.button = button
 end
 
-local textColor = {0,0,0,1.0}
-local inactiveColor = {0.6, 0.6, 0.6, 1.0}
-local activeColor =   {0.9, 0.9, 0.9, 1.0}
-
 class.TabView(View)
 
 ---
@@ -35,6 +31,11 @@ function TabView:_init(bounds)
     self:super(bounds)
     self.tabs = {} --TabViewItem
     self.currentTabIndex = 0
+
+    self.textColor = {0,0,0,1.0}
+    self.inactiveColor = {0.75, 0.75, 0.75, 1.0}
+    self.activeColor =   {0.75, 0.9, 0.9, 1.0}
+
     local barHeight = math.min(0.1, bounds.size.height/10.0)
     local barBounds = bounds:copy()
         :insetEdges(0, 0, 0, bounds.size.height - barHeight, 0, 0)
@@ -56,10 +57,10 @@ function TabView:addTab(name, view)
     -- add the new button
     local button = ui.Button(buttonBounds)
     button.label:setText(name)
-    button.label:setColor(textColor)
-    button:setColor(inactiveColor)
+    button.label:setColor(self.textColor)
+    button:setColor(self.inactiveColor)
     button.onActivated = function()
-        self:switchToTabIndex(#tabCount)
+        self:switchToTabIndex(tabCount)
     end
     self.tabBar:addSubview(button)
     self.tabBar:layout()
@@ -67,6 +68,10 @@ function TabView:addTab(name, view)
     -- add the new item
     local item = TabViewItem(view, name, button)
     table.insert(self.tabs, item)
+
+    -- resize the view to fit the tab view
+    local viewBounds = self.bounds:copy():insetEdges(0, 0, self.tabBar.bounds.size.height, 0, 0, 0)
+    view:setBounds(viewBounds)
 
     -- if we don't have a selected tab, make the new one the selected one
     if #self.tabs == 1 then
@@ -78,14 +83,18 @@ end
 function TabView:switchToTabIndex(index)
     local newTab = self.tabs[index]
     local oldTab = self.currentTabIndex > 0 and self.tabs[self.currentTabIndex] or nil
+    if index == self.currentTabIndex then
+        return
+    end
+    self.currentTabIndex = index
 
     if newTab then
         self:addSubview(newTab.view)
-        newTab.button:setColor(activeColor)
+        newTab.button:setColor(self.activeColor)
     end
     if oldTab then 
         oldTab.view:removeFromSuperview()
-        oldTab.button:setColor(inactiveColor)
+        oldTab.button:setColor(self.inactiveColor)
     end
 end
 
